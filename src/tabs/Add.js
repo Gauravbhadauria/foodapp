82e7d2164311f009640ad38fe5730f287f539843
Gaurav Bhadauria <gauravbhadauria@Gauravs-MacBook-Pro.local>
2022-12-03 16:22:24 +0530

    first done

--- src/tabs/Add.js
+++ src/tabs/Add.js
@@ -4,34 +4,141 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  PermissionsAndroid,
+  Image,
+  ScrollView,
 } from 'react-native';
-import React from 'react';
-
+import React, {useState} from 'react';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
 const Add = () => {
+  const [imageData, setImageData] = useState(null);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [discountPrice, setDiscountPrice] = useState(0);
+  const [description, setDescription] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: 'Cool Photo App Camera Permission',
+          message:
+            'Cool Photo App needs access to your camera ' +
+            'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the camera');
+        openGallery();
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  const openGallery = async () => {
+    const result = await launchImageLibrary({mediaType: 'photo'});
+    if (result.didCancel) {
+    } else {
+      console.log(result);
+      setImageData(result);
+    }
+  };
+
+  const uplaodImage = async () => {
+    const reference = storage().ref(imageData.assets[0].fileName);
+    const pathToFile = imageData.assets[0].uri;
+    // uploads file
+    await reference.putFile(pathToFile);
+    const url = await storage()
+      .ref(imageData.assets[0].fileName)
+      .getDownloadURL();
+    console.log(url);
+    uploadItem(url);
+  };
+
+  const uploadItem = url => {
+    firestore()
+      .collection('items')
+      .add({
+        name: name,
+        price: price,
+        discountPrice: discountPrice,
+        description: description,
+        imageUrl: url + '',
+      })
+      .then(() => {
+        console.log('User added!');
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Add Item</Text>
+    <ScrollView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Add Item</Text>
+        </View>
+
+        {imageData !== null ? (
+          <Image
+            source={{uri: imageData.assets[0].uri}}
+            style={styles.imageStyle}
+          />
+        ) : null}
+        <TextInput
+          placeholder="Enter Item Name"
+          style={styles.inputStyle}
+          value={name}
+          onChangeText={text => setName(text)}
+        />
+        <TextInput
+          placeholder="Enter Item Price"
+          style={styles.inputStyle}
+          value={price}
+          onChangeText={text => setPrice(text)}
+        />
+        <TextInput
+          placeholder="Enter Item Discount Price"
+          style={styles.inputStyle}
+          value={discountPrice}
+          onChangeText={text => setDiscountPrice(text)}
+        />
+        <TextInput
+          placeholder="Enter Item Description"
+          style={styles.inputStyle}
+          value={description}
+          onChangeText={text => setDescription(text)}
+        />
+        <TextInput
+          placeholder="Enter Item Image URL"
+          style={styles.inputStyle}
+          value={imageUrl}
+          onChangeText={text => setImageUrl(text)}
+        />
+        <Text style={{alignSelf: 'center', marginTop: 20}}>OR</Text>
+        <TouchableOpacity
+          style={styles.pickBtn}
+          onPress={() => {
+            requestCameraPermission();
+          }}>
+          <Text>Pick Image From Gallery</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.uploadBtn}
+          onPress={() => {
+            uplaodImage();
+          }}>
+          <Text style={{color: '#Fff'}}>Upload Item</Text>
+        </TouchableOpacity>
       </View>
-      <TextInput placeholder="Enter Item Name" style={styles.inputStyle} />
-      <TextInput placeholder="Enter Item Price" style={styles.inputStyle} />
-      <TextInput
-        placeholder="Enter Item Discount Price"
-        style={styles.inputStyle}
-      />
-      <TextInput
-        placeholder="Enter Item Description"
-        style={styles.inputStyle}
-      />
-      <TextInput placeholder="Enter Item Image URL" style={styles.inputStyle} />
-      <Text style={{alignSelf: 'center', marginTop: 20}}>OR</Text>
-      <TouchableOpacity style={styles.pickBtn}>
-        <Text>Pick Image From Gallery</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.uploadBtn}>
-        <Text style={{color:'#Fff'}}>Upload Item</Text>
-      </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -81,5 +188,13 @@ const styles = StyleSheet.create({
     marginTop: 20,
     justifyContent: 'center',
     alignItems: 'center',
+    marginBottom: 70,
+  },
+  imageStyle: {
+    width: '90%',
+    height: 200,
+    borderRadius: 10,
+    alignSelf: 'center',
+    marginTop: 20,
   },
 });
